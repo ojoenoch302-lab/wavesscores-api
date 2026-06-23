b1ad3769f0585768efe3f6bc93e2ec2528ede605
ojoenoch302-lab <ojoenoch302@gmail.com>
2026-06-23 17:40:53 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+from engine.ticket_builder import generate_ticket
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Wavesscores API is running"
+
+
+@app.route("/generate-ticket", methods=["GET"])
+def generate():
+
+    user_id = request.args.get("user_id", "guest")
+    mode = request.args.get("mode", "safe")
+
+    ticket, odds = generate_ticket(user_id=user_id, mode=mode)
+
+    result = []
+
+    for pick in ticket:
+        result.append({
+            "match": pick["Match"],
+            "pick": pick["Pick"],
+            "odds": float(pick["Odds"]),
+            "confidence": int(pick["Confidence"])
+        })
+
+    return jsonify({
+        "user": user_id,
+        "mode": mode,
+        "total_odds": round(odds, 2),
+        "ticket": result
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
